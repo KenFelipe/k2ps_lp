5b430b06dea5dc5a4396282dd5fed59436e2c1ac
Felipe Kenzo <Kenzo12152@gmail.com>
2021-01-29 15:03:53 -0300

    create dynamic thead only

--- compiled/table.js
+++ compiled/table.js
@@ -1,48 +1,138 @@
 "use strict";
 
-var tables_container = document.getElementById('tables'); ////////////////////////////////////////
-
-var table_section = document.createElement('div');
-table_section.id = 'imovel-table';
-var table = document.createElement('table');
-
-function createTableHead() {
+function createTableHead(table) {
   var thead = document.createElement('thead');
   var tr = document.createElement('tr');
-  var colsN = 2;
-
-  for (var i = 0; i < colsN; i++) {
+  table.head.forEach(function (theadTd) {
     var td = document.createElement('td');
-    td.innerHTML = "thead-".concat(i);
+    td.innerHTML = theadTd;
     tr.appendChild(td);
     thead.appendChild(tr);
+  });
+  return thead;
+}
+
+function createTableBody(row, column) {
+  var tbody = document.createElement('tbody');
+
+  for (var r = 1; r < row; r++) {
+    var tr = document.createElement('tr');
+
+    for (var c = 0; c < column; c++) {
+      var td = document.createElement('td'); // mudar aqui
+
+      td.innerHTML = "td-column-".concat(c);
+      tr.appendChild(td);
+      thead.appendChild(tr);
+    }
+
+    tbody.appendChild(tr);
   }
 
-  table.appendChild(thead);
+  return tbody;
 }
 
-var func = function func() {
-  return false;
+function init(config) {
+  var tablesContainer = document.getElementById('tables'); // return config length
+
+  var tableTypes = Object.keys(TABLES_CONFIG); // sep types
+
+  tableTypes.forEach(function (tableType, tableTypeIdx) {
+    var tableTypeSection = document.createElement('div');
+    console.log(tableType);
+    console.log(Object.keys(TABLES_DATA[tableType]));
+    Object.keys(TABLES_DATA[tableType]).forEach(function (tabName, tabIdx) {
+      var table = document.createElement('table'); // const thead = createTableHead(tableType, tabName)
+
+      var thead = createTableHead(TABLES_DATA[tableType][tabName]);
+      table.appendChild(thead);
+      tableTypeSection.appendChild(table);
+    });
+    tablesContainer.appendChild(tableTypeSection);
+  }); // for(let typeIdx = 0; typeIdx < tableTypesQty; typeIdx++) {}
+  // for config length {
+  //     table_section = document.createElement('div')
+  //     // sep tabs
+  //     for tabs {
+  //         const table = document.createElement('table')
+  //         const thead = createTableHead(.column)
+  // 
+  //         // need get row
+  //         // const row = getrow(array)
+  //         const tbody = createTableBody(.column)
+  //         table.appendChild(thead)
+  //         table.appendChild(tbody)
+  //         table_section.appendChild(table)
+  //     }
+  //     tables_container.appendChild(table_section)
+  // }
+
+  console.log(tablesContainer);
+}
+
+var TABLES_DATA = {
+  imovel: {
+    'Ate 240mil': {
+      head: ['thead', 'thead'],
+      body: [['R$55.000,00', 'R$349.00'], ['R$55.000,00', 'R$349.00'], ['R$55.000,00', 'R$349.00'], ['R$55.000,00', 'R$349.00'], ['R$55.000,00', 'R$349.00']]
+    },
+    'Ate 580mil': {
+      head: ['500mil', 'thead'],
+      body: [['R$55.000,00', 'R$349.00'], ['R$55.000,00', 'R$349.00'], ['R$55.000,00', 'R$349.00'], ['R$55.000,00', 'R$349.00'], ['R$55.000,00', 'R$349.00']]
+    },
+    'Ate 100mil': {
+      head: ['thead', 'thead'],
+      body: [['R$55.000,00', 'R$349.00'], ['R$55.000,00', 'R$349.00'], ['R$55.000,00', 'R$349.00'], ['R$55.000,00', 'R$349.00'], ['R$55.000,00', 'R$349.00']]
+    }
+  } // automovel: { 
+  //     auto: '100$' 
+  // }
+
 };
+var TABLES_CONFIG = {
+  imovel: {
+    tabs: 3,
+    columns: 2
+  } // automovel: {
+  //     tabs: 3,
+  //     columns: 5
+  // }
 
-function createTableBody() {
-  var tbody = document.createElement('tbody');
-  Array(10).fill(0).map(function (_, i) {
-    var tr = document.createElement('tr');
-    var td1 = document.createElement('td');
-    td1.innerHTML = 'R$55.000,00';
-    var td2 = document.createElement('td');
-    td2.innerHTML = 'R$349,00';
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tbody.appendChild(tr);
-  });
-  table.appendChild(tbody);
-}
-
-createTableHead();
-createTableBody();
-table_section.appendChild(table);
-tables_container.appendChild(table_section); ////////////////////////////////////////
-
-console.log(tables_container);
+};
+init(); // const imovel = [
+//     [ 
+//         ['thead', 'thead'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//     ],
+//     [
+//         ['thead', 'thead'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//     ],
+//     [
+//         ['thead', 'thead'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//     ]
+// ]
+// const tables_container = document.getElementById('tables')
+// ////////////////////////////////////////
+// const table_section = document.createElement('div')
+// table_section.id = 'imovel-table'
+// const table = document.createElement('table')
+// const thead = createTableHead(2)
+// const tbody = createTableBody(5, 2)
+// table.appendChild(thead)
+// table.appendChild(tbody)
+// table_section.appendChild(table)
+// tables_container.appendChild(table_section)
--- src/table.js
+++ src/table.js
@@ -1,53 +1,173 @@
-const tables_container = document.getElementById('tables')
-////////////////////////////////////////
-
-const table_section = document.createElement('div')
-table_section.id = 'imovel-table'
-
-const table = document.createElement('table')
-
-function createTableHead() {
+function createTableHead(table) {
     const thead = document.createElement('thead')
     const tr = document.createElement('tr')
 
-    const colsN = 2
-    for(let i = 0; i < colsN; i++) {
+    table.head.forEach(theadTd => {
         const td = document.createElement('td')
-        td.innerHTML = `thead-${i}`
+        td.innerHTML = theadTd
 
         tr.appendChild(td)
         thead.appendChild(tr)
-    }
-    table.appendChild(thead)
+    })
+    return thead
 }
 
-const func = () => false
-
-function createTableBody() {
+function createTableBody(row, column) {
     const tbody = document.createElement('tbody')
 
-    Array(10).fill(0).map((_, i) => {
+    for(let r = 1; r < row; r++) {
         const tr = document.createElement('tr')
 
-        const td1 = document.createElement('td')
-        td1.innerHTML = 'R$55.000,00'
+        for(let c = 0; c < column; c++) {
+            const td = document.createElement('td')
 
-        const td2 = document.createElement('td')
-        td2.innerHTML = 'R$349,00'
+            // mudar aqui
+            td.innerHTML = `td-column-${c}`
 
-        tr.appendChild(td1)
-        tr.appendChild(td2)
+            tr.appendChild(td)
+            thead.appendChild(tr)
+        }
 
         tbody.appendChild(tr)
-    })
-    table.appendChild(tbody)
+    }
+    return tbody
 }
 
-createTableHead()
-createTableBody()
+function init(config) {
+    const tablesContainer = document.getElementById('tables')
 
-table_section.appendChild(table)
-tables_container.appendChild(table_section)
+    // return config length
+    const tableTypes = Object.keys(TABLES_CONFIG)
+    // sep types
+    tableTypes.forEach((tableType, tableTypeIdx) => {
+        const tableTypeSection = document.createElement('div')
 
-////////////////////////////////////////
-console.log(tables_container)
+        console.log(tableType)
+        console.log(Object.keys(TABLES_DATA[tableType]))
+
+        Object.keys(TABLES_DATA[tableType]).forEach((tabName, tabIdx) => {
+            const table = document.createElement('table')
+            // const thead = createTableHead(tableType, tabName)
+            const thead = createTableHead(TABLES_DATA[tableType][tabName])
+            table.appendChild(thead)
+            tableTypeSection.appendChild(table)
+        })
+
+        tablesContainer.appendChild(tableTypeSection)
+    })
+    // for(let typeIdx = 0; typeIdx < tableTypesQty; typeIdx++) {}
+    // for config length {
+    //     table_section = document.createElement('div')
+
+    //     // sep tabs
+    //     for tabs {
+    //         const table = document.createElement('table')
+    //         const thead = createTableHead(.column)
+    // 
+    //         // need get row
+    //         // const row = getrow(array)
+    //         const tbody = createTableBody(.column)
+    //         table.appendChild(thead)
+    //         table.appendChild(tbody)
+    //         table_section.appendChild(table)
+    //     }
+    //     tables_container.appendChild(table_section)
+    // }
+
+    console.log(tablesContainer)
+}
+
+const TABLES_DATA = { 
+    imovel: {
+        'Ate 240mil': {
+            head: ['thead', 'thead'],
+            body: [ 
+                ['R$55.000,00', 'R$349.00'],
+                ['R$55.000,00', 'R$349.00'],
+                ['R$55.000,00', 'R$349.00'],
+                ['R$55.000,00', 'R$349.00'],
+                ['R$55.000,00', 'R$349.00'],
+            ],
+        },
+        'Ate 580mil': {
+            head: ['500mil', 'thead'],
+            body: [
+                ['R$55.000,00', 'R$349.00'],
+                ['R$55.000,00', 'R$349.00'],
+                ['R$55.000,00', 'R$349.00'],
+                ['R$55.000,00', 'R$349.00'],
+                ['R$55.000,00', 'R$349.00'],
+            ],
+        },
+        'Ate 100mil': {
+            head: ['thead', 'thead'],
+            body: [
+                ['R$55.000,00', 'R$349.00'],
+                ['R$55.000,00', 'R$349.00'],
+                ['R$55.000,00', 'R$349.00'],
+                ['R$55.000,00', 'R$349.00'],
+                ['R$55.000,00', 'R$349.00'],
+            ]
+        }
+    }
+    // automovel: { 
+    //     auto: '100$' 
+    // }
+}
+
+const TABLES_CONFIG = {
+    imovel: {
+        tabs: 3,
+        columns: 2
+    },
+    // automovel: {
+    //     tabs: 3,
+    //     columns: 5
+    // }
+}
+
+init()
+
+// const imovel = [
+//     [ 
+//         ['thead', 'thead'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//     ],
+//     [
+//         ['thead', 'thead'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//     ],
+//     [
+//         ['thead', 'thead'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//         ['R$55.000,00', 'R$349.00'],
+//     ]
+// ]
+
+// const tables_container = document.getElementById('tables')
+// ////////////////////////////////////////
+
+// const table_section = document.createElement('div')
+// table_section.id = 'imovel-table'
+
+// const table = document.createElement('table')
+
+
+// const thead = createTableHead(2)
+// const tbody = createTableBody(5, 2)
+// table.appendChild(thead)
+// table.appendChild(tbody)
+
+// table_section.appendChild(table)
+// tables_container.appendChild(table_section)
