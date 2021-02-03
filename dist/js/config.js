1532ef6fa7527be8e00c115b0ebf306c3ef58230
Felipe Kenzo <Kenzo12152@gmail.com>
2021-02-03 01:01:15 -0300

    end tab responsive scss

--- dist/js/config.js
+++ dist/js/config.js
@@ -1,14 +1,38 @@
 "use strict";
 
+// buttons
 var buttons = document.getElementsByClassName(CLASSNAME_CONFIG.selectTablesButton);
 var btnClassName = {
-  4: 'buttons-2-column',
-  5: 'buttons-3-column',
-  "default": 'buttons-4-column'
+  1: '',
+  2: '',
+  3: '',
+  4: 'column-buttons-2',
+  5: 'column-buttons-3',
+  "default": 'column-buttons-4'
 };
 var btnClass = btnClassName[btnClassName.hasOwnProperty(buttons.length) ? buttons.length : 'default'];
 console.log(btnClass);
 
 for (var btnIndex = 0; btnIndex < buttons.length; btnIndex++) {
   buttons.item(btnIndex).classList.add(btnClass);
+} // tabs
+
+
+var tabs = document.getElementsByClassName('tabbar__tab'); // console.log(tabs)
+// DEBUG
+// const column_tab = 5
+
+var column_tab = 'more-5'; // const column_tab = 'less-5'
+
+var testIndex = 3;
+var tabClass = "tabs-".concat(column_tab);
+
+for (var tabIdx = 0; tabIdx < tabs.length; tabIdx++) {
+  tabs.item(tabIdx).classList.add(tabClass);
 }
+
+buttons.item(0).classList.remove('active');
+buttons.item(testIndex).classList.add('active');
+document.getElementsByClassName('tabbar').item(0).classList.remove('active');
+document.getElementsByClassName('tabbar').item(testIndex).classList.add('active');
+document.getElementsByClassName('tabs__bottom-bar').item(0).classList.add(tabClass);
--- src/js/config.js
+++ src/js/config.js
@@ -1,9 +1,13 @@
+// buttons
 const buttons = document.getElementsByClassName(CLASSNAME_CONFIG.selectTablesButton)
 
 const btnClassName = {
-    4: 'buttons-2-column',
-    5: 'buttons-3-column',
-    default: 'buttons-4-column'
+    1: '',
+    2: '',
+    3: '',
+    4: 'column-buttons-2',
+    5: 'column-buttons-3',
+    default: 'column-buttons-4'
 }
 
 const btnClass = btnClassName[btnClassName.hasOwnProperty(buttons.length) ? buttons.length : 'default']
@@ -12,3 +16,25 @@ console.log(btnClass)
 for(let btnIndex = 0; btnIndex < buttons.length; btnIndex++) {
     buttons.item(btnIndex).classList.add(btnClass)
 }
+
+// tabs
+const tabs = document.getElementsByClassName('tabbar__tab')
+// console.log(tabs)
+
+// DEBUG
+// const column_tab = 5
+const column_tab = 'more-5'
+// const column_tab = 'less-5'
+const testIndex = 3
+
+const tabClass = `tabs-${column_tab}`
+for(let tabIdx = 0; tabIdx < tabs.length; tabIdx++) {
+    tabs.item(tabIdx).classList.add(tabClass)
+}
+
+buttons.item(0).classList.remove('active')
+buttons.item(testIndex).classList.add('active')
+document.getElementsByClassName('tabbar').item(0).classList.remove('active')
+document.getElementsByClassName('tabbar').item(testIndex).classList.add('active')
+
+document.getElementsByClassName('tabs__bottom-bar').item(0).classList.add(tabClass)
