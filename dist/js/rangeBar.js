80aee4adec5cc2ec93e3f04069cf6c8189fb599d
Felipe Kenzo <Kenzo12152@gmail.com>
2021-02-04 14:48:46 -0300

    rangeb test

--- dist/js/rangeBar.js
+++ dist/js/rangeBar.js
@@ -7,4 +7,25 @@ rangeSlider.addEventListener('input', function () {
   var bulletPosition = "".concat(rangeSlider.value / rangeSlider.max * 578, "px");
   rangeBullet.style.left = bulletPosition;
   document.getElementsByClassName('progress').item(0).style.width = bulletPosition;
+}); /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var parcela = document.getElementById('parcela');
+parcela.addEventListener('click', function () {
+  credito.classList.remove('active');
+  parcela.classList.add('active');
+});
+var credito = document.getElementById('credito');
+credito.addEventListener('click', function () {
+  parcela.classList.remove('active');
+  credito.classList.add('active');
+});
+var rangeBar = document.getElementById('range-bar');
+console.log(rangeBar);
+rangeBar.addEventListener('input', function () {
+  // const width = '20px'
+  var progress = (rangeBar.value - rangeBar.min) / (rangeBar.max - rangeBar.min) * (rangeBar.offsetWidth - 40) + 20;
+  console.log(progress, rangeBar.offsetWidth); // const width = '80%'
+
+  var width = "".concat(progress, "px");
+  document.getElementById('range-bar-progress').style.width = width;
 });
--- src/js/rangeBar.js
+++ src/js/rangeBar.js
@@ -9,3 +9,30 @@ rangeSlider.addEventListener('input', () => {
 
     document.getElementsByClassName('progress').item(0).style.width = bulletPosition
 })
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const parcela = document.getElementById('parcela')
+
+parcela.addEventListener('click', () => {
+    credito.classList.remove('active')
+    parcela.classList.add('active')
+})
+
+const credito = document.getElementById('credito')
+
+credito.addEventListener('click', () => {
+    parcela.classList.remove('active')
+    credito.classList.add('active')
+})
+
+const rangeBar = document.getElementById('range-bar')
+console.log(rangeBar)
+
+rangeBar.addEventListener('input', () => {
+    // const width = '20px'
+    const progress = (rangeBar.value - rangeBar.min) / (rangeBar.max - rangeBar.min) * (rangeBar.offsetWidth - 40) + 20
+    console.log(progress, rangeBar.offsetWidth)
+    // const width = '80%'
+    const width = `${progress}px`
+    document.getElementById('range-bar-progress').style.width = width
+})
